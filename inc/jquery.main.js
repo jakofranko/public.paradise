31d39af5df5ba806250782853608be2b8b0c48f5
Devine Lu Linvega <aliceffekt@gmail.com>
2016-08-01 11:36:05 -0700

    *

--- inc/jquery.main.js
+++ inc/jquery.main.js
@@ -1,11 +1,56 @@
-$(document).ready(function() {
+var queue = [];
+
+$(document).ready(function()
+{
+	var response_example = "* You are {{the unknown geomaitre}} in {{your pretty microwave}}.\n& your full name is the unknown geomaitre\n# create a teapot\n- {{your panther}}\n- {{your liger}}\n- {{your golden machine}}\n- {{your automated whale}}";
+	
+	
+
+	$(document).on('keydown', 'input', function(e) {
+	    if(e.which == 13) {
+	    	responded(response_example);
+	        return false;
+	    }
+	});
+
+	updateTerminal();
 });
 
-$(window).resize(function() {
+function responded(response)
+{
+	var lines = response.split("\n");
+	for (i = 0; i < lines.length; i++) { 
+	    queue.push(lines[i]);
+	}
+}
+
+function markup(text)
+{
+	templates = text.match(/\{\{(.*?)\}\}/g)
+	return false;
+}
+
+function updateTerminal()
+{
+	if(queue.length > 0){
+		$("terminal").append("<response>"+queue[0]+"</response>");
+		queue.splice(0, 1);	
+	}
+	
+	setTimeout(function(){ updateTerminal(); }, 100);
+}
+
+$(window).resize(function()
+{
+
 });
 
-$(window).load(function() {
+$(window).load(function()
+{
+
 });
 
-$(window).scroll(function () {
+$(window).scroll(function()
+{
+
 });
