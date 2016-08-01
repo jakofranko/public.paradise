275801d78ac7516db4873692444125fa5457c157
Devine Lu Linvega <aliceffekt@gmail.com>
2016-08-01 12:07:49 -0700

    *

--- inc/jquery.main.js
+++ inc/jquery.main.js
@@ -1,20 +1,25 @@
 var queue = [];
 
 $(document).ready(function()
-{
-	var response_example = "* You are {{the unknown geomaitre}} in {{your pretty microwave}}.\n& your full name is the unknown geomaitre\n# create a teapot\n- {{your panther}}\n- {{your liger}}\n- {{your golden machine}}\n- {{your automated whale}}";
-	
-	
-
+{	
+	apiCall("connect");
 	$(document).on('keydown', 'input', function(e) {
 	    if(e.which == 13) {
-	    	responded(response_example);
+	    	apiCall($("input").val());
+	    	$("input").val("");
 	        return false;
 	    }
 	});
-
 	updateTerminal();
 });
+
+function apiCall(command)
+{
+	console.log("API > "+command);
+	responded("> <span class='user'>"+command+"</span>");
+	var response = "* You are {{the unknown geomaitre}} in {{your pretty microwave}}.\n& Today is your lucky day, you are finaly free from chains. Today is your lucky day, you are finaly free from chains.\n# create a teapot\n- {{Your panther}}\n- {{Your liger}}\n- {{your golden machine}}\n- {{your automated whale}}";
+	responded(response);
+}
 
 function responded(response)
 {
@@ -26,17 +31,28 @@ function responded(response)
 
 function markup(text)
 {
-	templates = text.match(/\{\{(.*?)\}\}/g)
-	return false;
+	var rune = text.charAt(0);
+	text = text.replace(rune+" ","<rune>"+rune+"</rune>");
+
+	templates = text.match(/\{\{(.*?)\}\}/g);
+
+	if(templates == null){ return text; }
+
+	for (i = 0; i < templates.length; i++) { 
+	    text = text.replace(templates[i],"<action>"+templates[i]+"</action>");
+	}
+	text = text.split("{{").join("");
+	text = text.split("}}").join("");
+
+	return text;
 }
 
 function updateTerminal()
 {
 	if(queue.length > 0){
-		$("terminal").append("<response>"+queue[0]+"</response>");
+		$("terminal").append("<response>"+markup(queue[0])+"</response>");
 		queue.splice(0, 1);	
 	}
-	
 	setTimeout(function(){ updateTerminal(); }, 100);
 }
 
